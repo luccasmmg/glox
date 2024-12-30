751475416cd00358be11cf3fbe6c9133e7d4b91d
Luccas Mateus <luccasmmg@gmail.com>
2024-12-30 17:53:50 -0300

    Fix clock

--- globals.go
+++ globals.go
@@ -4,14 +4,14 @@ import "time"
 
 type Time struct{}
 
-func (t *Time) arity() int {
+func (t Time) Arity() int {
   return 0
 }
 
-func (t *Time) call(interpreter *Interpreter, arguments []interface{}) (interface{}, error) {
+func (t Time) Call(interpreter *Interpreter, arguments []interface{}) (interface{}, error) {
   return float64(time.Now().Unix()), nil
 } 
 
-func (t *Time) String() string {
+func (t Time) String() string {
   return "<native fn>"
 }
